0054c4b4ecec68534f9f33dec1f51994bdb1be49
Miranda Hammarstedt <mh223tt@student.lnu.se>
2016-12-29 00:14:44 +0100

    Adding functionality to create new notes and remove all, getting old notes etc.

--- client/source/js/Remember.js
+++ client/source/js/Remember.js
@@ -34,43 +34,92 @@ Remember.prototype.constructor = Remember;
 
 /**
  * Creates a new note.
+ *
+ * @param {Boolean} notFirst - Whether or not the created note is the first or not.
  */
-Remember.prototype.new = function() {
+Remember.prototype.new = function(notFirst) {
+    if (notFirst) {
+        let container = this.div.querySelector(".content");
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+
+        this.notes = [];
+    }
+
     let template = document.querySelector("#remember").content;
     let content = document.importNode(template, true);
-    document.getElementById(this.id).querySelector(".content").appendChild(content);
+    this.div.querySelector(".content").appendChild(content);
 
-    this.setMenu();
-
-    let input = document.getElementById(this.id).querySelector(".note-input");
-    document.getElementById(this.id).querySelector("button").addEventListener("click", function() {
+    let input = this.div.querySelector(".note-input");
+    this.div.querySelector("button").addEventListener("click", function() {
         if (!input.value) {
             input.classList.add("redbg");
+            this.message.textContent = "You need to write an item for the list.";
         } else {
             input.classList.remove("redbg");
+            this.message.textContent = "";
             this.add(input.value);
             input.value = "";
         }
     }.bind(this));
 
-    this.dropdown.textContent = "Save";
-    this.dropdown.addEventListener("click", function(event) {
-        event.preventDefault();
-        this.save();
-    }.bind(this));
+    if (!notFirst) {
+        this.setMenu();
+        if (storage.get("notes") !== null) {
+            this.save(true);
+        }
+
+        this.dropdown.textContent = "Save";
+        this.dropdown.addEventListener("click", function(event) {
+            event.preventDefault();
+            if (this.div.querySelectorAll(".note p").length > 1) {
+                this.save();
+                this.message.textContent = "";
+            } else {
+                this.message.textContent = "Note is empty.";
+            }
+        }.bind(this));
+    }
 };
 
 /**
- * Sets the dropdown menu containing notes.
+ * Sets the different dropdown menus.
  */
 Remember.prototype.setMenu = function() {
-    let element = document.getElementById(this.id).querySelector(".menulink");
+    let element = this.div.querySelector(".menulink");
     let menuClone = element.cloneNode(true);
     element.parentNode.appendChild(menuClone);
 
-    let newLink = document.getElementById(this.id).querySelectorAll(".menulink")[1];
+    let newLink = this.div.querySelectorAll(".menulink")[1];
     newLink.firstElementChild.textContent = "Notes";
     newLink.querySelector(".dropdown").removeChild(newLink.querySelector(".dropdown a"));
+
+    for (let i = 0; i < 2; i += 1) {
+        let dropdownClone = this.div.querySelectorAll(".menulink")[0].querySelector(".dropdown a").cloneNode(true);
+        this.div.querySelectorAll(".menulink")[0].lastElementChild.appendChild(dropdownClone);
+    }
+
+    let dropdownLinks = this.div.querySelectorAll(".menulink")[0].querySelectorAll(".dropdown a");
+    dropdownLinks[1].textContent = "New";
+    dropdownLinks[2].textContent = "Delete All";
+
+    dropdownLinks[1].addEventListener("click", function(event) {
+        event.preventDefault();
+        this.new(true);
+    }.bind(this));
+
+    dropdownLinks[2].addEventListener("click", function(event) {
+        event.preventDefault();
+        localStorage.removeItem("notes");
+
+        let container = newLink.lastElementChild;
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+
+        this.new(true);
+    }.bind(this));
 };
 
 /**
@@ -79,33 +128,57 @@ Remember.prototype.setMenu = function() {
  * @param {String} input - User input from element.
  */
 Remember.prototype.add = function(input) {
-    let noteElem = document.getElementById(this.id).querySelectorAll(".note p")[0].cloneNode(true);
+    let noteElem = this.div.querySelectorAll(".note p")[0].cloneNode(true);
     noteElem.textContent = input;
-    document.getElementById(this.id).querySelector(".note").appendChild(noteElem);
+    this.div.querySelector(".note").appendChild(noteElem);
 
     this.notes.push(input);
 };
 
 /**
- * Saves current note to local storage.
+ * Saves current note to local storage or gets old notes.
+ *
+ * @param {Boolean} oldNotes - Whether or not there are old notes in local storage.
  */
-Remember.prototype.save = function() {
-    storage.set("notes", this.notes);
+Remember.prototype.save = function(oldNotes) {
+    let newLink;
+    let dropdownLink;
 
-    let newLink = document.getElementById(this.id).querySelectorAll(".menulink")[1];
-    let dropdownClone = document.getElementById(this.id).querySelectorAll(".menulink")[0].
-        querySelector(".dropdown a").cloneNode(true);
-    newLink.lastElementChild.appendChild(dropdownClone);
+    let addMenuNote = function() {
+        newLink = this.div.querySelectorAll(".menulink")[1];
+        let dropdownClone = this.div.querySelectorAll(".menulink")[0].
+            querySelector(".dropdown a").cloneNode(true);
+        newLink.lastElementChild.appendChild(dropdownClone);
 
-    let dropdownLink = newLink.querySelector(".dropdown").lastElementChild;
-    dropdownLink.textContent = "Note" + this.nr;
-    let nr = this.nr;
-    this.nr += 1;
+        dropdownLink = newLink.querySelector(".dropdown").lastElementChild;
+        dropdownLink.textContent = "Note" + this.nr;
+        this.nr += 1;
 
-    dropdownLink.addEventListener("click", function(event) {
-        event.preventDefault();
-        this.get(nr);
-    }.bind(this));
+        dropdownLink.addEventListener("click", function(event) {
+            event.preventDefault();
+            let nr = event.target.textContent.charAt(event.target.textContent.length - 1);
+            if (nr === 0) {
+                nr = 9;
+            }
+
+            this.get(nr);
+        }.bind(this));
+    }.bind(this);
+
+    let notes = (storage.get("notes") === null) ? 0 : storage.get("notes").notes;
+    if (oldNotes) {
+        notes.forEach(function() {
+            addMenuNote();
+        }.bind(this));
+    } else {
+        if (notes === 0 || notes.length < 4) {
+            storage.set("notes", this.notes);
+            addMenuNote();
+            this.new(true);
+        } else {
+            this.message.textContent = "You already have 5 saved notes.";
+        }
+    }
 };
 
 /**
@@ -117,7 +190,7 @@ Remember.prototype.get = function(nr) {
     let notes = storage.get("notes").notes;
     let noteContent = notes[(nr - 1)];
 
-    let container = document.getElementById(this.id).querySelector(".note");
+    let container = this.div.querySelector(".note");
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
@@ -127,7 +200,7 @@ Remember.prototype.get = function(nr) {
     container.appendChild(content);
 
     noteContent.forEach(function(current) {
-        let noteElem = document.getElementById(this.id).querySelectorAll(".note p")[0].cloneNode(true);
+        let noteElem = this.div.querySelectorAll(".note p")[0].cloneNode(true);
         noteElem.textContent = current;
         container.appendChild(noteElem);
     }.bind(this));
