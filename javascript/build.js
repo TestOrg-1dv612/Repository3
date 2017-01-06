3b467ed9d4964fa4728ae1ba0a6df4353c485376
Miranda Hammarstedt <mh223tt@student.lnu.se>
2017-01-06 15:46:01 +0100

    Attempting service worker.

--- javascript/build.js
+++ javascript/build.js
@@ -1111,6 +1111,17 @@ function init() {
     getDate(document.querySelector("#date"));
     desktopClock(document.querySelector("#clock"));
     setInterval(desktopClock, 5000);
+
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function() {
+            navigator.serviceWorker.register('/sw.js').then(function(registration) {
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }).catch(function(err) {
+                console.log('ServiceWorker registration failed: ', err);
+            });
+        });
+    }
+
 }
 
 /**
